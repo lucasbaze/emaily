ca7ed4e07edc6368932bf9f39c9fd2ab7e1737d9
Lucas Bazemore <lucasabazemore@gmail.com>
2019-07-25 16:04:13 -0500

    added db, mongoose, and auth

--- prod/server/index.js
+++ prod/server/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { mongoURI } = require('./config/keys.js');
+require('./models/User.js');
+require('./services/passport.js');
+
+mongoose.connect(mongoURI, { useNewUrlParser: true });
+
 const app = express();
 
+require('./routes/authRoutes.js')(app);
+
+//https://www.udemy.com/node-with-react-fullstack-web-development/learn/lecture/7603018?start=644#bookmarks
+// Same as the line above
+// const authRoutes = require('./routes/authRoutes.js');
+// authRoutes(app);
+
 app.get('/', (req, res) => {
-    return res.send(console.log(req.app));
+    return res.send('Hello There!');
 });
 
 //Heroku Dynamic Port Binding
